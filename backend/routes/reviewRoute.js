89a1f3a86ff21ef1c5f3d02a03dbc253eff71d6f
Joy Sameza <samgaming500@gmail.com>
2023-06-06 16:17:47 +0100

    new commit

--- backend/routes/reviewRoute.js
+++ backend/routes/reviewRoute.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { reviewModel } = require("../models/reviewModels");
 
-
-router.post('/new-review', (req, res, next) => {
-    console.log(req.body);
-    res.end("done");
-    next();
+router.post("/new-review", (req, res, next) => {
+  const dt = {
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.review,
+  };
+  const newReview = new reviewModel(dt);
+  newReview
+    .save()
+    .then(() => {
+      console.log("SAved!!");
+      res.end("done");
+    })
+    .catch((err) => {
+        console.error(err);
+        res.end("error");
+    });
+  next();
 });
 
 module.exports = router;
