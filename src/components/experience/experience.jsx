b25afd1f5a9824303c1500e946cb3cd15fe431eb
Joy Sameza <101399643+Joy-sameza@users.noreply.github.com>
2024-03-25 01:04:07 +0100

    Clean up experience.jsx

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -80,85 +80,13 @@ export default function Experience() {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             {frontendContent}
-            {/* <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Intermidiate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Boostrap</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Tailwind</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>ReactJs</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article> */}
           </div>
         </div>
-
-        {/* End of frntend section */}
 
         <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
             {backendContent}
-            {/* <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>NodeJs</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Intermidiate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MySQL</h4>
-                <small className='text-light'>Intermidiate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PHP</h4>
-                <small className='text-light'>Basic</small>
-              </div>
-            </article> */}
           </div>
         </div>
       </div>
